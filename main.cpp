2bc141ca751c5108a38cf0979bd1f144b1e42801
tony.chromebook <tonetheman@gmail.com>
2021-08-04 09:18:12 -0500

    still working on web compile not working yet

--- main.cpp
+++ main.cpp
@@ -10,26 +10,35 @@ const int W = 400;
 const int H = 400;
 #define SIZE 10
 
+B board(10,10);
+
+void UpdateAndDraw() {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    board.render();
+
+
+    EndDrawing();
+}
+
 int main() {
 
-    B board(10,10);
+    //B board(10,10);
     board.init(5);
     board.score();
     board.repr();
     
     InitWindow(W,H,"minesweeper");
+#ifdef PLATFORM_WEB
+    emscripten_set_main_loop(UpdateAndDraw,0,1);
+#else
     SetTargetFPS(30);
 
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        board.render();
-
-
-        EndDrawing();
+        UpdateAndDraw();
     }
-
+#endif
     CloseWindow();
 
     return 0;
--- makefile
+++ makefile
@@ -8,3 +8,14 @@ minesweeper : main.cpp b.cpp
 
 clean :
 	rm -f minesweeper
+	rm -f main.o
+	rm -f b.o
+
+RAYLIB_PATH=../raylibHTML5
+
+web :
+	emcc -c main.cpp b.cpp \
+	-I $(RAYLIB_PATH)/src -D PLATFORM_WEB \
+	-s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 \
+	--shell-file $(RAYLIB_PATH)/src/shell.html \
+	$(RAYLIB_PATH)/src/libraylib.a
